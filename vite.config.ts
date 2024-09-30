830e8978814db23c030a2e2acdaccc6931b485f4
Anton <anton@Antons-Laptop.local>
2024-09-30 10:59:34 +0700

    init

---
+++ vite.config.ts
@@ -0,0 +1,39 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      '@constants': path.resolve(__dirname, './src/constants'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@store': path.resolve(__dirname, './src/store'),
+      '@ui': path.resolve(__dirname, './src/ui'),
+      '@api': path.resolve(__dirname, './src/api'),
+      '@hooks': path.resolve(__dirname, './src/hooks'),
+      '@icons': path.resolve(__dirname, './src/icons'),
+      '@utils': path.resolve(__dirname, './src/utils'),
+      '@helpers': path.resolve(__dirname, './src/helpers')
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: '@import "@constants/variables.scss";'
+      }
+    }
+  },
+  preview: {
+    port: 8081,
+    strictPort: true
+  },
+  server: {
+    port: 8081,
+    strictPort: true,
+    host: true,
+    origin: 'http://0.0.0.0:8081'
+  }
+});
